b16014119ed96968218ad9e3b9f5cb56116d64dc
GlueGunStanley <kasonstanley@gmail.com>
2024-12-14 10:09:45 -0500

    Update sv_main.lua

--- Emergency-Alert-System/config.lua
+++ Emergency-Alert-System/config.lua
@@ -4,6 +4,12 @@ Config.Settings = {
     voice = "paul", -- paul or tom
     volume = 0.5,
     cooldown = 60,
-    onfoot = false,
-    onbycycle = false
+    onfoot = true,
+    onbycycle = true
+}
+
+Config.WeatherRadios = {
+    vector4(0.0, 0.0, 0.0, 0.0),
+    vector4(0.0, 0.0, 0.0, 0.0),
+    vector4(0.0, 0.0, 0.0, 0.0)
 }
--- Emergency-Alert-System/server/sv_main.lua
+++ Emergency-Alert-System/server/sv_main.lua
@@ -20,11 +20,12 @@ RegisterCommand(eas, function(source, args, rawCommand)
         TriggerClientEvent('chat:addMessage', source, {args = { "Error", "Invalid arguments. Usage: /eas {type}" }})
         return
     end
-    if type ~= thunderstorm or type ~= snowstorm or type ~= tornado or type ~= tsunami or type ~= flood or type ~= hurricane then
+    if type ~= thunderstorm or type ~= snowstorm or type ~= tornado or type ~= tsunami or type ~= flood or type ~= hurricane or type ~= nuke then
         TriggerClientEvent('chat:addMessage', source, {args = { "Error", "Arguments out of bounds. Ensure you are entering a valid alert type." }})
         return
     end
     TriggerClientEvent("cl:EAS:TriggerOnAll", -1, type)
+    Active = true
 end, false)
 
 RegisterNetEvent("sv:EAS:Callback")
@@ -33,7 +34,26 @@ RegisterNetEvent("sv:EAS:Callback", function()
         return
     end
     Received = true
-    Wait(Config.Settings.cooldown * 1000 or 5000)
+    Wait(Config.Settings.cooldown * 1000)
     Active = false
     Received = false
+end)
+
+RegisterNetEvent("sv:EAS:ExternalTrigger")
+RegisterNetEvent("sv:EAS:ExternalTrigger", function(arg)
+    if Active then 
+        print("Error", "An alert is already active. Please wait.")
+        return
+    end
+    local type = tostring(arg)
+    if not type then
+        print("Error", "Invalid arguments. Usage: /eas {type}")
+        return
+    end
+    if type ~= thunderstorm or type ~= snowstorm or type ~= tornado or type ~= tsunami or type ~= flood or type ~= hurricane or type ~= nuke then
+        print("Error", "Arguments out of bounds. Ensure you are entering a valid alert type.")
+        return
+    end
+    TriggerClientEvent("cl:EAS:TriggerOnAll", -1, type)
+    Active = true
 end)
